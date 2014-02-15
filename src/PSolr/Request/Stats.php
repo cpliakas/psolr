fe2e634fa62a1f52dc35b34f13b610f8cc454b08
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2014-02-15 12:13:30 -0500

    Fixed #7: Add a request object for the Stats component.

---
+++ src/PSolr/Request/Stats.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PSolr\Request;
+
+/**
+ * @see http://wiki.apache.org/solr/StatsComponent
+ */
+class Stats extends SolrRequest implements ComponentInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function preMergeParams(SolrRequest $request) {}
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init()
+    {
+        $this->stats();
+    }
+
+    /**
+     * @param bool $stats
+     *
+     * @return \PSolr\Request\Stats
+     *
+     * @see http://wiki.apache.org/solr/StatsComponent
+     */
+    public function stats($stats = true)
+    {
+        return $this->set('stats', (bool) $stats);
+    }
+
+    /**
+     * @param string $field
+     *
+     * @return \PSolr\Request\Stats
+     *
+     * @see http://wiki.apache.org/solr/StatsComponent
+     */
+    public function addField($field)
+    {
+        return $this->add('stats.field', $field);
+    }
+
+    /**
+     * @param string $facet
+     *
+     * @return \PSolr\Request\Stats
+     *
+     * @see http://wiki.apache.org/solr/StatsComponent
+     */
+    public function addField($facet)
+    {
+        return $this->add('stats.facet', $facet);
+    }
+}
